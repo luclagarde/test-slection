554e4f0f380e7a5ee959dc76208c1deb9fea7f9f
luclagarde <75084549+luclagarde@users.noreply.github.com>
2022-04-06 20:51:37 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,67 @@
+input.onButtonPressed(Button.A, function () {
+    if (Choix == 0 || Choix == 3) {
+        Choix = 1
+        basic.showNumber(Choix)
+    } else if (Choix == 1) {
+        Choix = 2
+        basic.showNumber(Choix)
+    } else {
+        Choix = 3
+        basic.showNumber(Choix)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (Choix == 1) {
+        soundExpression.giggle.playUntilDone()
+    } else if (Choix == 2) {
+        soundExpression.soaring.playUntilDone()
+    } else {
+        soundExpression.twinkle.playUntilDone()
+    }
+    Fleche()
+})
+function Fleche () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . # . . .
+        . . # . .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+}
+let Choix = 0
+music.setBuiltInSpeakerEnabled(true)
+music.setVolume(255)
+Choix = 0
+Fleche()
+basic.forever(function () {
+	
+})
